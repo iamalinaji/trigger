a4cb0a3c92e5f503402374987a1ba1e3e00a3832
iamalinaji <anm33918@gmail.com>
2023-07-22 09:32:18 +0330

    Initializing Project.

---
+++ src/main/java/com/alinaji/trigger/TriggerApplication.java
@@ -0,0 +1,13 @@
+package com.alinaji.trigger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriggerApplication.class, args);
+	}
+
+}
